53e0d450f1e094fa4b1857aa86481d632125a8f2
Eddy Dezuraud <eddy@gedivote.fr>
2024-04-02 10:35:48 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'), // Update path here
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
@@ -15,6 +16,24 @@ module.exports = {
         ],
     },
     optimization: {
-        minimize: false
-    }
+        minimize: true
+    },
+    resolve: {
+        fallback: {
+            "stream": require.resolve("stream-browserify"),
+            "string_decoder": require.resolve("string_decoder/"),
+            "timers": require.resolve("timers-browserify"),
+            buffer: require.resolve("buffer/")
+        }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "index.html"),
+                    to: path.resolve(__dirname, "public"),
+                },
+            ],
+        }),
+    ]
 };
